4324c735ff29cbeacc4c2d2182103c500b8dab5a
ItsJustHack <eliott.leboeuf@gmail.com>
2021-10-23 15:19:55 +0200

    Modification sur PC-Fixe

--- config.h
+++ config.h
@@ -9,13 +9,15 @@ static const int swallowfloating    = 1;        /* 1 means swallow floating wind
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"FontAwesome:size=11", "monospace:size=11", "JoyPixels:pixelsize=11:antialias=true:autohint=true"  };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = {"FontAwesome:size=10", "monospace:size=10", "JoyPixels:pixelsize=10"  };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+
 #include "/home/eliott/.cache/wal/colors-wal-dwm.h"
 #include <X11/XF86keysym.h>
 /* tagging */
